126dbdb7dd4b24b6ce0303f8d6372cc8d5b08e24
Aksel Skauge Mellbye <aksel.mellbye@silabs.com>
2025-01-31 12:40:16 +0100

    simplicity_sdk: Patch pa_conversions_efr32.c to not conflict
    Zephyr defines the MAX() macro in util.h, which causes a conflict
    with a locally defined macro of the same name.
    
    Signed-off-by: Aksel Skauge Mellbye <aksel.mellbye@silabs.com>

--- scripts/patch_simplicity_sdk.sh
+++ scripts/patch_simplicity_sdk.sh
@@ -12,5 +12,8 @@ sed -i '' "s/   first/first/" simplicity_sdk/platform/common/inc/sl_common.h
 # Replace legacy Kconfig option name
 sed -i '' "s/CONFIG_SOC_FAMILY_EXX32/__ZEPHYR__/" simplicity_sdk/platform/emlib/inc/em_ramfunc.h
 
+# Rename MAX macro conflicting with Zephyr macro
+sed -i '' "s/MAX(/_SL_MAX(/" simplicity_sdk/platform/radio/rail_lib/plugin/pa-conversions/pa_conversions_efr32.c
+
 # Add Zephyr OS abstraction for crypto
 sed -i '' "s/#\(if defined(SL_CATALOG_MICRIUMOS_KERNEL_PRESENT)\)/#if defined(__ZEPHYR__)\n  #include \"sli_psec_osal_zephyr.h\"\n  #define SLI_PSEC_THREADING\n#el\1/" simplicity_sdk/platform/security/sl_component/sli_psec_osal/inc/sli_psec_osal.h
